a0d6e36c1352cf601f72294d62a324bb16d9a746
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-03-14 21:22:13 +0400

    Refactor code

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-
-namespace nasocket_client
+﻿namespace nasocket_client
 {
 	internal class Program
 	{
@@ -12,32 +7,9 @@ namespace nasocket_client
 			const string ServerAddress = "127.0.0.1";
 			const int Port = 11177;
 
-			IPEndPoint endPoint = new(IPAddress.Parse(ServerAddress), Port);
-			Socket clientSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-			try {
-				Console.WriteLine("Connecting to server...");
-				clientSocket.Connect(endPoint);
-				Console.WriteLine("Connected.");
-				while (true) {
-					string? message = Console.ReadLine();
-					if (message == null) {
-						Console.WriteLine("Stop reading from console");
-						break;
-					}
-					byte[] messageBytes = Encoding.UTF32.GetBytes(message);
-					clientSocket.Send(messageBytes);
-					Console.WriteLine("Message was sent");
-
-					byte[] buffer = new byte[1024];
-					int receivedBytes = clientSocket.Receive(buffer);
-					string response = Encoding.UTF32.GetString(buffer);
-					Console.WriteLine($"Response from server: {response}");
-				}
-			} catch (Exception exception) {
-				Console.WriteLine($"Fatal error! {exception.Message}.");
-			} finally {
-				clientSocket.Close();
+			using Connection connection = new(ServerAddress, Port);
+			if (connection.TryConnect()) {
+				connection.StartDialog();
 			}
 		}
 	}
